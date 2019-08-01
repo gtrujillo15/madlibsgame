cab14b227efa4b3ebb0d05d8fae5bb3144fd9cec
Gabriela T <gtrujillo15@gmail.com>
2019-07-31 21:33:52 -0700

    started a small repo for a madlib game. Created text field and button element. Tomorrow I'd like to look into when the button is clicked, the preset text will come up along with the noun that was written

---
+++ script.js
@@ -0,0 +1,12 @@
+// Section for adding a noun
+// Text field for the noun
+var nounText = document.createElement("textarea");
+nounText.innerHTML = "";
+document.body.appendChild(nounText);
+
+// Text field for the button
+var btn = document.createElement("Button");
+btn.innerHTML = "Enter";
+document.body.appendChild(btn);
+
+// Upon clicking in noun, save noun
