3404ca87d0a93f8164dd391aec1896e513471c5d
Gabriela T <gtrujillo15@gmail.com>
2019-08-05 21:25:15 -0700

    saved noun to local storage

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ btn.innerHTML = "Enter";
 document.body.appendChild(btn);
 
 // Upon clicking in noun, save noun
+btn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var nounChoice = nounText.value;
+    localStorage.setItem('noun', nounChoice);
+})
+
+var nounEnd = localStorage.getItem('noun');
+var madLib = "I wanted to laugh but then I saw " + nounEnd;
+document.body.appendChild(madLib);
