bd2dcd404b5f8e1a8b1f9b8d5a6db792735f5e2e
Gabriela T <gtrujillo15@gmail.com>
2019-11-24 20:26:07 -0800

    got it to work! i have it to where it can print one sentence with a word but am already thinking of adding more

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-var container = document.getElementsByClassName("container-fluid");
-// Section for adding a noun
-// Text field for the noun
-var nounText = document.createElement("textarea");
-nounText.innerHTML = "";
-document.body.appendChild(nounText);
 
-// Text field for the button
+// Creating Text Box and Appending to MadLibs div
+var textBox = document.createElement("textarea");
+textBox.innerHTML = "";
+textBox.placeholder = "Enter a noun";
+document.getElementById("madlibs").appendChild(textBox);
+
+
+
+// Creating Button and Appending to MadLibs div
 var btn = document.createElement("Button");
 btn.innerHTML = "Enter";
-document.body.appendChild(btn);
+newline = document.createElement("br");
+document.getElementById("madlibs").appendChild(newline);
+document.getElementById("madlibs").appendChild(btn);
+
+
 
 // Upon clicking in noun, save noun
 btn.addEventListener('click', function(event) {
     event.preventDefault();
-    var nounChoice = nounText.value;
-    localStorage.setItem('noun', nounChoice);
+    // Setting item in local storage
+    var wordChoice = textBox.value;
+    localStorage.setItem('noun', wordChoice);
+    // Grab item from local stoarage and placing into sentence
+    var wordEnd = localStorage.getItem('noun');
+    var container = document.createElement('p');
+
+    // Create madlib sentence and attach word to end
+    container.innerHTML = "I wanted to laugh but then I saw " + wordEnd;
+
+    // Append to madlibs section in HTML
+    document.getElementById("madlibs").appendChild(container);
 })
 
-var nounEnd = localStorage.getItem('noun');
-var madLib = "I wanted to laugh but then I saw " + nounEnd;
-document.body.container.appendChild(madLib);
